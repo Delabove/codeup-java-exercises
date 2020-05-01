e12462a8726ef740b9566c236716a44eb6a225fd
DeLayne LaBove <delayne.labove@gmail.com>
2020-05-01 09:26:23 -0500

    First commit of syntax exercise

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -1,5 +1,9 @@
 public class HelloWorld {
     public static void main( String args []){
         System.out.println("Hello Java World!");
+
+        int myFavoriteNumber = 1128;
+        System.out.println("My favorite number is " + myFavoriteNumber);
+
     }
 }
