fed6b67e45f445ad7b05f4003614eb8bdfa86d7c
DeLayne LaBove <delayne.labove@gmail.com>
2020-05-01 09:29:24 -0500

    Commit two of sytax exercise

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -2,8 +2,16 @@ public class HelloWorld {
     public static void main( String args []){
         System.out.println("Hello Java World!");
 
+//        Create an int variable named myFavoriteNumber and assign your favorite
+//        number to it, then print it out to the console.
+
         int myFavoriteNumber = 1128;
         System.out.println("My favorite number is " + myFavoriteNumber);
 
+     //Create a String variable named myString and assign a string value
+        // to it, then print the variable out to the console.
+
+        String myString = "This is my string.";
+        System.out.println("This is my string value: " + myString);
     }
 }
