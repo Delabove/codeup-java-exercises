1ba28378e9d2537cd703a8aa6660735926b970da
DeLayne LaBove <delayne.labove@gmail.com>
2020-05-04 12:15:12 -0500

    FizzBuzz exercise

--- src/ControlFlowExercises.java
+++ src/ControlFlowExercises.java
@@ -5,8 +5,24 @@ public class ControlFlowExercises {
 //        System.out.println("Hello");
 
 
-        for(int i = 100; i >= -10; i-=5) {
+        for(int i = 1; i <= 100; i++) {
+
             System.out.println(i);
+
+            if(i % 3 == 0 && i % 5 == 0){
+
+                System.out.println("FizzBuzz");
+
+            }else if(i % 3 == 0){
+
+                System.out.println("Fizz");
+
+            } else if(i % 5 == 0){
+
+                System.out.println("Buzz");
+
+
+            }
 
         }
 
