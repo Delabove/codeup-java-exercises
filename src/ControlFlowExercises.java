bce62f446cc1e62465158ef8f8ebf3d4245286aa
DeLayne LaBove <delayne.labove@gmail.com>
2020-05-04 15:31:02 -0500

    walkthrough commit

--- src/ControlFlowExercises.java
+++ src/ControlFlowExercises.java
@@ -4,7 +4,42 @@ public class ControlFlowExercises {
 
     public static void main(String args[]) {
 
-//        System.out.println("Hello");
+//        System.out.println("Control Flow Exercise");
+
+//        int i = 5;
+//
+//        while (i <= 15){
+//            System.out.print(i + " ");
+//            i++;
+//        }
+//
+//        int counter = 0;
+//
+//        do {
+//            System.out.println(counter);
+//            counter += 2;
+//        } while (counter <= 100);
+
+//        int counter = 100;
+//
+//        do {
+//            System.out.println(counter);
+//            counter -= 5;
+//        } while (counter <= 100);
+
+//
+//        int counter = 2L;
+//        do{
+//            System.out.println(counter);
+//            counter *= counter;
+//        } while (counter < 1000000L);
+//        for(int counter =100; counter >= -10; counter -=5){
+//            System.out.println(counter);
+//        }
+
+//        for(long counter = 2L; counter < 1000000L; counter *= counter){
+//            System.out.println(counter);
+//        }
 
 
         for(int i = 1; i <= 100; i++) {
