4989acbbfdad087c922b0783e7cc57f812b80337
DeLayne LaBove <delayne.labove@gmail.com>
2020-05-04 11:21:01 -0500

    While loop Java

---
+++ src/ControlFlowExercises.java
@@ -0,0 +1,15 @@
+public class ControlFlowExercises {
+
+    public static void main(String args[]) {
+
+//        System.out.println("Hello");
+        int i = 5;
+
+        while (i <= 15){
+            System.out.println("Less than or equal to 15: " + i);
+            i++;
+        }
+
+
+    }
+}
