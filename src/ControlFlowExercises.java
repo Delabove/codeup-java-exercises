34f4a21a70c6b51cb6f4751b808f80fd4cbc392c
DeLayne LaBove <delayne.labove@gmail.com>
2020-05-04 14:03:11 -0500

    Table of powers exercise

--- src/ControlFlowExercises.java
+++ src/ControlFlowExercises.java
@@ -1,3 +1,5 @@
+import java.util.Scanner;
+
 public class ControlFlowExercises {
 
     public static void main(String args[]) {
@@ -7,7 +9,7 @@ public class ControlFlowExercises {
 
         for(int i = 1; i <= 100; i++) {
 
-            System.out.println(i);
+                 System.out.println(i);
 
             if(i % 3 == 0 && i % 5 == 0){
 
@@ -25,6 +27,31 @@ public class ControlFlowExercises {
             }
 
         }
+
+        Scanner sc = new Scanner(System.in);
+        System.out.println("What number would you like to go up to?");
+        int number =  sc.nextInt();
+        System.out.println("Here's your table!");
+            int NMAX = 3;
+            double userMax = number;
+
+            for(int n = 1; n<= NMAX; n++){
+//                System.out.printf("%10d", n);
+            }
+        System.out.println();
+            for (int n = 1; n <= NMAX;n++){
+//                System.out.printf("%10s", "x ");
+
+            }
+        System.out.println();
+
+            for(double x = 1; x <= userMax; x++){
+                for(int n = 1; n <= NMAX; n++) {
+                    System.out.printf("%10.0f", Math.pow(x, n));
+
+                }
+                System.out.println();
+            }
 
 
     }
