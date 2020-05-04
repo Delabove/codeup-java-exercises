8e41d5158dedb265487c89ce92b87995a0a1dcda
DeLayne LaBove <delayne.labove@gmail.com>
2020-05-04 11:41:28 -0500

    For loop to 15

--- src/ControlFlowExercises.java
+++ src/ControlFlowExercises.java
@@ -3,14 +3,10 @@ public class ControlFlowExercises {
     public static void main(String args[]) {
 
 //        System.out.println("Hello");
-        long i = 2L;
 
-        do{
 
-            System.out.println("Do While: " + i);
-            i *= i;
-        } while (i < 1000000L); {
-            System.out.println("Do while complete!");
+        for(int i = 5; i <= 15; i++) {
+            System.out.println(i);
 
         }
 
