662da1cc41ea83ae760c24660bd1bf5db6bf5fbf
DeLayne LaBove <delayne.labove@gmail.com>
2020-05-04 11:31:46 -0500

    Do while decrementing by 5

--- src/ControlFlowExercises.java
+++ src/ControlFlowExercises.java
@@ -3,13 +3,13 @@ public class ControlFlowExercises {
     public static void main(String args[]) {
 
 //        System.out.println("Hello");
-        int i = 0;
+        int i = 100;
 
         do{
 
             System.out.println("Do While: " + i);
-            i++;
-        } while (i <= 100); {
+            i-= 5;
+        } while (i >= -10); {
             System.out.println("Do while complete!");
 
         }
