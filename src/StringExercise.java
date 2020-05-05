1be8debc43e605a786b05d93e3dd37446b5d4d09
DeLayne LaBove <delayne.labove@gmail.com>
2020-05-05 11:39:22 -0500

    Walkthrough

--- src/StringExercise.java
+++ src/StringExercise.java
@@ -1,20 +1,18 @@
 public class StringExercise {
 
-    public static void main(String args[]){
+    public static void main(String[] args){
 
 
-        String noEducation = "We don't need no education\n" +
-                "We don't need no thought control";
+        String noEducation = "We don't need no education\nWe don't need no thought control";
         System.out.println(noEducation);
 
-        String sInsideofS = "Check \"this\" out!, \"s inside of \"s!";
-        System.out.println(sInsideofS);
+        String sInsideOfS = "Check \"this\" out!, \"s inside of \"s!";
+        System.out.println(sInsideOfS);
 
         String mainDrive = "In windows, the main drive is usually C:\\";
         System.out.println(mainDrive);
 
-        String backslashes = "I can do backslashes \\, double backslashes \\\\,\n" +
-                "and the amazing triple backslash \\\\\\!";
+        String backslashes = "I can do backslashes \\, double backslashes \\\\,\nand the amazing triple backslash \\\\\\!";
         System.out.println(backslashes);
     }
 
