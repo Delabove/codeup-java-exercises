2db34d00e0bacd90da3ad87d7deef611f5e790f1
DeLayne LaBove <delayne.labove@gmail.com>
2020-05-05 09:49:33 -0500

    1 of Strings exercise

---
+++ src/StringExercise.java
@@ -0,0 +1,17 @@
+public class StringExercise {
+
+    public static void main(String args[]){
+
+
+        String noEducation = "We don't need no education\n" +
+                "We don't need no thought control";
+        System.out.println(noEducation);
+
+        String sInsideofS = "Check \"this\" out!, \"s inside of \"s!";
+        System.out.println(sInsideofS);
+
+        String mainDrive = "In windows, the main drive is usually C:\\";
+        System.out.println(mainDrive);
+    }
+
+}
