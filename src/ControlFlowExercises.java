976ac7b68d452a06a426527b480d89eacde19dae
DeLayne LaBove <delayne.labove@gmail.com>
2020-05-05 09:38:37 -0500

    Walkthrough

--- src/ControlFlowExercises.java
+++ src/ControlFlowExercises.java
@@ -62,38 +62,80 @@ public class ControlFlowExercises {
             }
 
         }
-
+//
         Scanner sc = new Scanner(System.in);
+//
+//        System.out.println("What number would you like to go up to?");
+//
+//        int number = sc.nextInt();
+//
+//        System.out.println("Here's your table!");
+//
+//        int tableColumn = 3;
+//        double userMax = number;
+//
+//        for (int n = 1; n <= tableColumn; n++) {
+//
+//        }
+//        System.out.println();
 
-        System.out.println("What number would you like to go up to?");
+//douglas answer
+        3. table of powers
+//        Scanner scanner = new Scanner(System.in);
+//
+//        boolean userContinues = true;
+//
+//        do {
+//            System.out.println("What number would you like to go up to? ");
+//            int userInt = scanner.nextInt();
+//            System.out.println();
+//            System.out.println("Here is your table!");
+//            System.out.println();
+//            System.out.println("number | squared | cubed");
+//            System.out.println("------ | ------- | -----");
+//            for(int i = 1; i <= userInt; i++){
+//                System.out.format("%-6d", i);
+//                System.out.print(" | ");
+//                System.out.format("%-7d", i * i);
+//                System.out.print(" | ");
+//                System.out.print(i * i * i);
+//                System.out.println();
+//            }
+//            System.out.print("Would you like to enter another number (y/n)? ");
+//            String userResponse = scanner.next();
+//            if (!userResponse.equalsIgnoreCase("y")) {
+//                userContinues = false;
+//            }
+//        } while (userContinues);
 
-        int number = sc.nextInt();
+        boolean userContinues = true;
 
-        System.out.println("Here's your table!");
+        do{
+            System.out.println("What number would you like to go up to?");
 
-        int tableColumn = 3;
-        double userMax = number;
-
-        for (int n = 1; n <= tableColumn; n++) {
-
-        }
-        System.out.println();
-
-        for (int n = 1; n <= tableColumn; n++) {
-
-
-        }
-        System.out.println();
-
-        for (double x = 1; x <= userMax; x++) {
-            for (int n = 1; n <= tableColumn; n++) {
-                System.out.printf("%10.0f", Math.pow(x, n));
-
-            }
+            int userInt = sc.nextInt();
             System.out.println();
-        }
+            System.out.println("Here is your table!");
+            System.out.println();
 
+//PUTS FOR LOOP HERE INSIDE OF DO WHILE
 
+        } while (userContinues);
+
+//
+//        for (int n = 1; n <= tableColumn; n++) {
+//
+//
+//        }
+//        System.out.println();
+//
+//        for (double x = 1; x <= userMax; x++) {
+//            for (int n = 1; n <= tableColumn; n++) {
+//                System.out.printf("%10.0f", Math.pow(x, n));
+//
+//            }
+//            System.out.println();
+//        }
 
         Scanner userInput = new Scanner(System.in);
 
@@ -117,6 +159,36 @@ public class ControlFlowExercises {
             System.out.println("Your score is " + numberGrade + " which is an F.");
         }
 
+//DOUGLAS EXAMPLE
+        Scanner scanner = new Scanner(System.in);
+
+        boolean anotherGrade = true;
+
+        do {
+            System.out.print("Please enter a numerical grade from 0 to 100 ");
+
+            int numericGrade = scanner.nextInt();
+
+            if (numericGrade >= 88) {
+                System.out.println("A");
+            } else if (numericGrade >= 80) {
+                System.out.println("B");
+            } else if (numericGrade >= 67) {
+                System.out.println("C");
+            } else if (numericGrade >= 60) {
+                System.out.println("D");
+            } else {
+                System.out.println("F");
+            }
+
+            System.out.print("Do you wish to enter a new grade? ");
+            String userResponse = scanner.next();
+            if (!userResponse.equalsIgnoreCase("y")) {
+                anotherGrade = false;
+            }
+        } while (anotherGrade);
+
 
     }
+
 }
