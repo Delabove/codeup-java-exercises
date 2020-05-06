c4943bc863116779865f9464e0ae4d5f87a7e4f7
DeLayne LaBove <delayne.labove@gmail.com>
2020-05-06 08:13:21 -0500

    1 of Methods Exercise

---
+++ src/MethodsExercises.java
@@ -0,0 +1,35 @@
+public class MethodsExercises {
+
+    public static void main(String [] args){
+
+        System.out.println(addition(1, 2));
+        System.out.println(subtraction(1, 2));
+        System.out.println(multiplication(1, 2));
+        System.out.println(division(4, 2));
+    }
+
+
+//Create four separate methods. Each will perform an arithmetic operation:
+//
+//Addition
+
+    public static int addition(int a, int b) {
+        return a + b;
+    }
+
+    //Subtraction
+    public static int subtraction(int a, int b) {
+        return a - b;
+    }
+    //Multiplication
+    public static int multiplication(int a, int b) {
+        return a * b;
+    }
+    //Division
+    public static int division(int a, int b) {
+        return a / b;
+    }
+
+
+
+}
