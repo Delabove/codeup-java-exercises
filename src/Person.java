00c6f4a849f2dc3079f739bf744fe1a500f67ef0
DeLayne LaBove <delayne.labove@gmail.com>
2020-05-07 14:26:48 -0500

    First commit of Exercise

---
+++ src/Person.java
@@ -0,0 +1,27 @@
+public class Person {
+
+    private String name;
+
+
+    // returns the person's name
+    public String getName(){
+
+    }
+    // changes the name property to the passed value
+    public void setName(String name){
+
+
+    }
+
+    // prints a message to the console using the person's name
+    public void sayHello(){
+
+
+    }
+
+
+
+
+
+
+}
