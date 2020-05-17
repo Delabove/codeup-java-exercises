82e98466419403908ca44b8c8839778202657a2d
DeLayne LaBove <delayne.labove@gmail.com>
2020-05-17 05:49:40 -0500

    Inheritance and Polymorphism review

--- src/Shapes/ShapesTest.java
+++ src/Shapes/ShapesTest.java
@@ -2,32 +2,16 @@ package Shapes;
 
 public class ShapesTest {
 
-    public static void main(String[] args) {
-//        Measurable myShape;
+        public static void main(String[] args) {
+
+        Rectangle box1 = new Rectangle(5, 4);
+            System.out.println("Area = "+box1.getArea());
+            System.out.println("Perimeter = "+box1.getPerimeter());
 
 
-        Measurable myShape = new Square(5);
-        System.out.println(myShape.getArea());
-        System.out.println(myShape.getPerimeter());
+            Rectangle box2 = new Square(5);
+            System.out.println("Area = "+box2.getArea());
+            System.out.println("Perimeter = "+box2.getPerimeter());
 
-        myShape = new Rectangle(2, 4);
-        System.out.println(myShape.getArea());
-        System.out.println(myShape.getPerimeter());
-
-
-
-//        Rectangle box1 = new Rectangle(4, 5);
-////        box1.length = 4;
-////        box1.width = 5;
-//
-//        System.out.println(box1.getPerimeter());
-//        System.out.println(box1.getArea());
-//
-//        Rectangle box2 = new Square(5);
-//        System.out.println(box2.getArea());
-//        System.out.println(box2.getPerimeter());
-
-    }
-
-
+        }
 }
--- src/Shapes/Square.java
+++ src/Shapes/Square.java
@@ -1,50 +1,94 @@
 package Shapes;
 
-public class Square extends Quadrilateral {
+public class Square extends Rectangle{
 
-    protected double side;
-public Square(double side){
-    super(side,side);
+    public Square(double side){
+        super(side, side);
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 }
 
-    @Override
-    public void setLength(double length) {
-        this.length = length;
-        this.width = length;
-    }
 
-    @Override
-    public void setWidth(double width) {
-        this.setLength(width);
-    }
 
-    @Override
-    public double getPerimeter() {
-        return 4 * super.length;
-    }
 
-    @Override
-    public double getArea() {
-        return Math.pow(super.width, 2);
 
-    }
-}
-//    public double side;
+
+
+
+
+
+
+
+
+
 //
-//    public Square(double side){
-//        super(side, side);
-//    }
-//
-//    public double getArea(){
-//        return Math.pow(super.width, 2);
-//    }
-//
-//    public double getPerimeter(){
-//        return 4 * super.length;
-//    }
-//
+//    protected double side;
+//public Square(double side){
+////    super(side,side);
+////}
 ////
-////    public static void main(String[] args) {
-////        Square sq1 = new Square(5);
-////        System.out.println("sq1.getPerimeter() = " + sq1.getPerimeter());
+////    @Override
+////    public void setLength(double length) {
+////        this.length = length;
+////        this.width = length;
 ////    }
+////
+////    @Override
+////    public void setWidth(double width) {
+////        this.setLength(width);
+////    }
+////
+////    @Override
+////    public double getPerimeter() {
+////        return 4 * super.length;
+////    }
+////
+////    @Override
+////    public double getArea() {
+////        return Math.pow(super.width, 2);
+////
+////    }
+////}
+//////    public double side;
+//////
+//////    public Square(double side){
+//////        super(side, side);
+//////    }
+//////
+//////    public double getArea(){
+//////        return Math.pow(super.width, 2);
+//////    }
+//////
+//////    public double getPerimeter(){
+//////        return 4 * super.length;
+//////    }
+//////
+////////
+////////    public static void main(String[] args) {
+////////        Square sq1 = new Square(5);
+////////        System.out.println("sq1.getPerimeter() = " + sq1.getPerimeter());
+////////    }
